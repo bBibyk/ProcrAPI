0b00f9dbee775af9d5a4f266272a284ab7b9e410
Bogdan Bibyk <bbibyk@gmail.com>
2025-04-03 16:53:22 +0200

    Project initialized

---
+++ procrapi/src/main/java/org/bem/procrapi/ProcrapiApplication.java
@@ -0,0 +1,13 @@
+package org.bem.procrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcrapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcrapiApplication.class, args);
+    }
+
+}
---
+++ procrapi/src/test/java/org/bem/procrapi/ProcrapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bem.procrapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcrapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
