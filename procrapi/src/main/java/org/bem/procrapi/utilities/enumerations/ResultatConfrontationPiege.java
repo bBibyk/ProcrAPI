2c83c026a31e1e287d615db5851b44cbe86a0e78
Bogdan Bibyk <bbibyk@gmail.com>
2025-06-03 03:03:47 +0200

    Comment

--- procrapi/src/main/java/org/bem/procrapi/utilities/enumerations/ResultatConfrontationPiege.java
+++ procrapi/src/main/java/org/bem/procrapi/utilities/enumerations/ResultatConfrontationPiege.java
@@ -1,6 +1,6 @@
 package org.bem.procrapi.utilities.enumerations;
 
 public enum ResultatConfrontationPiege {
-    SUCCES,
-    ECHEC
+    SUCCES, /*réussi et ne tombe pas dans le piège = procrastine*/
+    ECHEC /*échoue et travaille accidantellement*/
 }
